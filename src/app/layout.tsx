0a4fb6b92bf68072c5d5c1d03a7cae088b6edee2
dhdev <247596203+dhDev03@users.noreply.github.com>
2025-12-03 10:50:40 -0300

    .

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,9 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Public_Sans } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const PublicSans = Public_Sans({
+  variable: "--font-public_sans",
   subsets: ["latin"],
 });
 
@@ -24,10 +19,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body className={`${PublicSans} antialiased`}>
+            {children}
       </body>
     </html>
   );
